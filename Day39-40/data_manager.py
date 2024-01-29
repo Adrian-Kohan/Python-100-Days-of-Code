0031a98c112b8096a1c32ca1af1e6d0b003e4f87
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-01-29 19:48:55 +0330

    Update data_manager.py

--- Day39-40/data_manager.py
+++ Day39-40/data_manager.py
@@ -1,8 +1,36 @@
 import requests
 
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/********************************/flightDeals/prices/"
+SHEETY_USERS_ENDPOINT = "https://api.sheety.co/*********************************/flightDeals/users"
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
+
     def __init__(self):
-        self.endpoint = "https://api.sheety.co/3572120969531d095607c3bd34a0d71b/flightDeals/prices"
-        self.response = requests.get(url=self.endpoint)
-        self.data = self.response.json()
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
+
+    def get_customer_emails(self):
+        customers_endpoint = SHEETY_USERS_ENDPOINT
+        response = requests.get(url=customers_endpoint)
+        data = response.json()
+        self.customer_data = data["users"]
+        return self.customer_data
