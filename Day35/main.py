10b9cc9613664d20f727f3d304feeeaa17051e0e
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-01-29 18:59:54 +0330

    Update main.py

--- Day35/main.py
+++ Day35/main.py
@@ -1,6 +1,3 @@
-#Note! For the code to work you need to replace all the placeholders with
-#Your own details. e.g. account_sid, lat/lon, from/to phone numbers.
-
 import requests
 import os
 from twilio.rest import Client
@@ -8,7 +5,7 @@ from twilio.http.http_client import TwilioHttpClient
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = os.environ.get("OWM_API_KEY")
-account_sid = "YOUR ACCOUNT SID"
+account_sid = "************"
 auth_token = os.environ.get("AUTH_TOKEN")
 
 weather_params = {
