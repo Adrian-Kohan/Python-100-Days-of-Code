c8d6112b433b5895a9c8b90cef20fcfa65bf52e7
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-01-30 11:55:20 +0330

    Update main.py

--- Day48/main.py
+++ Day48/main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-chrome_driver_path = YOUR CHROME DRIVER PATH
+chrome_driver_path = "C:/Users/Adrian/Downloads/Compressed/chromedriver-win64/chromedriver-win64/chromedriver.exe"
 driver = webdriver.Chrome(chrome_driver_path)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
